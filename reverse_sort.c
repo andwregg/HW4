d84b14a8cc5313b53821311305d92a1a1f189a11
Andrew Wreggelsworth <andwregg@umich.edu>
2016-02-04 01:57:12 -0500

    P4 and P5

---
+++ reverse_sort.c
@@ -0,0 +1,8 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int compare(int a, int b) {
+	    return b - a;		//all that needs to change is the order that we compare the elements
+}
